6a5349bfd64af350c69243650f90e001ee4df8b5
burak-senel <buurak.senel@gmail.com>
2024-05-22 01:34:34 +0300

    2 task done 3 left

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,36 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    }
+    public static boolean isPalindrome(int sayi){
+sayi=Math.abs(sayi);
+char[] charArray=String.valueOf(sayi).toCharArray();
+String reversedSayi="";
+for(int i=charArray.length-1;i>=0;i--){
+reversedSayi+=charArray[i];
+}
+return reversedSayi.equalsIgnoreCase(String.valueOf(sayi));
+    }
+    public static boolean isPerfectNumber(int sayi){
+        if(sayi<0)
+            return false;
+        int total=0;
+        for(int i=1;i<=sayi/2;i++){
+            if(sayi % i == 0){
+                total+=i;
+            }
+        }
+        return sayi==total;
+
+    }
+    public static boolean numberToWords(int sayi){
+        sayi=Math.abs(sayi);
+        char[] charArray=String.valueOf(sayi).toCharArray();
+        String reversedSayi="";
+        for(int i=charArray.length-1;i>=0;i--){
+            reversedSayi+=charArray[i];
+        }
+        return reversedSayi.equalsIgnoreCase(String.valueOf(sayi));
     }
 }
